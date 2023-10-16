ff8e8d0b2f67dcc6bec0e32da2975490294ddb95
danja <danja@DESKTOP-MR9PF5H>
2023-10-16 23:41:56 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication6/ConsoleApplication6.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int main(void) {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hConsole, FOREGROUND_BLUE);
+    cout << "Hello, Савелий Ринсевич";
+    return 0;
+}
